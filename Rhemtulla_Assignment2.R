7feca3717b1139543f8420b925484313266d9ee0
imranrhem <imran.rhemtulla@gmail.com>
2022-06-08 15:33:36 -0400

    -corrected error handling for digits with length different than 3
    -corrected digit assignment

--- Rhemtulla_Assignment2.R
+++ Rhemtulla_Assignment2.R
@@ -11,19 +11,25 @@ user_number <- as.numeric(user_number)
 # Check to see if user input is valid (integer, 3 digits, non-negative)
 
   #If not valid, exit the program
-  if (is.na(user_number) || length((user_number) != 3 || user_number < 0)) {
+  if (is.na(user_number) || (floor(log10(user_number))+1) != 3 || user_number < 0) {
     print("Your input was invalid! Exiting program.")
   } else {
   
-  # Else store the digits into variables, storage 
-    digit_1 <- user_number[1]
-    digit_2 <- user_number[2]
-    digit_3 <- user_number[3]
+  # Else store the digits into variables, storage
+    
+    # Use modulus 10 to get the remainder ie) digit in the 1s spot
+    digit_3 <- user_number %% 10 
+    
+    # Use floor division to remove last digit and modulus to return value ie) digit in the 10s spot
+    digit_2 <- (user_number %/% 10) %% 10 
+    
+    # Use floor division to remove last two digits and modulus to get value ie) digit in the 100s spot
+    digit_1 <- ((user_number %/% 10) %/% 10) %% 10 
     
   # Calculate the sum of cubes of the digits
     cube_sum <- (digit_1^3 + digit_2^3 + digit_3^3)
     
-  # Check to see if the number is narcisstic and the print appropriate message
+  # Check to see if the number is narcissistic and the print appropriate message
     if (cube_sum == user_number) {
       print(paste(user_number, "is a narcissistic number."))
     } else {
